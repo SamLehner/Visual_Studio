d0178138764ec8f1cf7b09a6c2291f5dc12f7654
SamLehner <samblehner8@gmail.com>
2021-04-22 19:42:04 -0600

    Fixed Code Bug

--- VisualStudio_Website/JavaScript1.js
+++ VisualStudio_Website/JavaScript1.js
@@ -1,6 +1,6 @@
 ﻿var id = null;
 function myMove() {
-    var elem = document.getElementById("myAnimation");
+    var elem = document.getElementById("animation");
     var pos = 0;
     clearInterval(id);
     id = setInterval(frame, 10);
